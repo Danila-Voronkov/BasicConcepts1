42dd6a5b4dce7d9b97449cc0f483d276ccd40854
LAPTOP-95H61VHK\danil <danila.voronkov.2021@gmail.com>
2022-02-12 14:42:18 +0700

    Добавьте файлы проекта.

---
+++ BasicConcepts1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int Number = Convert.ToInt32(Console.ReadLine());
+      int Degree = Convert.ToInt32(Console.ReadLine());
+      int Answer = Number;
+
+      for (int Сounter = 1; Сounter < Degree; ++Сounter)
+      {
+        Answer *= Number;
+      }
+
+      Console.WriteLine(Answer);
+      Console.ReadKey();
+    }
+  }
+}
